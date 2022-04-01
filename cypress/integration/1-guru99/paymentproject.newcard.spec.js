b2e392fa81a0e33462aed6bfa02e0336bfa9a687
LBagrationi <lalibagrationi98@gmail.com>
2022-04-01 11:51:07 +0400

    commit4

---
+++ cypress/integration/1-guru99/paymentproject.newcard.spec.js
@@ -0,0 +1,31 @@
+describe("Payment gateway", () => {
+    beforeEach(() => {
+     cy.visit("https://demo.guru99.com/payment-gateway/index.php");
+});
+
+it("Payment page is correct", () => {
+    cy.get("#site-name").contains("a", "Demo Site").should("be.visible");
+    cy.get('img[alt="Guru99 Demo Sites"]').should("be.visible");
+    cy.get(".navbar").should("be.visible");
+    cy.get('a[href*="purchasetoy.php"]').contains('Guru99 Payment Gateway').should("be.visible");
+    cy.get('a[href*="purchasetoy.php"]').contains('Cart').should("be.visible");
+    cy.get('a[href*="cardnumber.php"]').contains('Generate Card Number').should("be.visible");
+    cy.get('a[href*="check_credit_balance.php"]').contains('Check Credit Card Limit').should("be.visible");
+
+    cy.get("h2").contains("Mother Elephant With Babies Soft Toy").scrollIntoView().should("be.visible");
+    cy.get('div[class="6u 12u$(small)"]').find('img').should('have.attr', 'src', 'images/Toy.jpg')
+    cy.get("form > .row > :nth-child(2)").scrollIntoView().should("be.visible");
+    cy.get('div[class="6u 12u$(small)"]').contains('h3', 'Price: $20').scrollIntoView().should("be.visible");
+    cy.get("h4").contains("Quantity: ").scrollIntoView().should("be.visible");
+    cy.get("select[name='quantity']").scrollIntoView().should("be.visible").select('1');
+    cy.get('input[value="Buy Now"]').scrollIntoView().should("be.visible");
+});
+
+it('New card creation', function () {
+        cy.visit('https://the-internet.herokuapp.com/windows')
+        cy.get('a[href*="cardnumber.php"]').invoke('removeAttr', 'target').click();
+        cy.url()
+            .should('include', 'https://demo.guru99.com/payment-gateway/cardnumber.php');
+        cy.get('h2').should('have.text', 'Here is your New Card')
+});
+});
